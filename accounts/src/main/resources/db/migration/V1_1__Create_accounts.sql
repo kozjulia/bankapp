5c6bbcb29db8dfaa0a11dec45100622ae8f86a68
Julia Kozlova <kozjuliajulia@yandex.ru>
2025-10-16 00:34:58 +0300

    fix README.md

--- accounts/src/main/resources/db/migration/V1_1__Create_accounts.sql
+++ accounts/src/main/resources/db/migration/V1_1__Create_accounts.sql
@@ -3,7 +3,7 @@ CREATE TABLE accounts
     id       BIGSERIAL  NOT NULL,
     user_id  BIGSERIAL  NOT NULL REFERENCES users (id),
     currency VARCHAR(3) NOT NULL,
-    value    DECIMAL    NOT NULL,
-    exists   boolean    NOT NULL,
+    value    DECIMAL    NOT NULL DEFAULT 0,
+    exists   boolean    NOT NULL DEFAULT true,
     CONSTRAINT accounts_pk PRIMARY KEY (id)
 );
